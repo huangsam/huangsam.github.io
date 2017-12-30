621d5c5d7073a6621665af36fb4295b029decccf
Samuel Huang <samhuang91@gmail.com>
2017-12-30 10:42:53 -0800

    Make site more minimal

--- bundle.js
+++ bundle.js
@@ -79,34 +79,32 @@ module.exports = __webpack_require__(2);
 
 
 var brand_colors = {
-  "github": "#24292e",
-  "facebook": "#3b5998",
-  "linkedin": "#0077b5",
-  "wordpress": "#21759b"
+    "github": "#24292e",
+    "facebook": "#3b5998",
+    "linkedin": "#0077b5",
+    "wordpress": "#21759b"
 };
 
 $(".theme").mouseover(changeTheme);
 $(".theme").mouseleave(originalTheme);
 
 function changeTheme() {
-  var brand = $(this).attr("id");
-  var color = brand_colors[brand];
+    var brand = $(this).attr("id");
+    var color = brand_colors[brand];
 
-  $("body").css("background-color", color);
-  $(".col p, h1").css("color", "white");
-  $(".col a").css("color", "white");
-  $("footer").css("background-color", "white");
-  $("footer span").css("color", color);
-  $("img").addClass("svg-invert");
+    $("body").css("background-color", color);
+    $("p, h1").css("color", "white");
+    $("a").css("color", "white");
+    $("footer span").css("color", "white");
+    $("code").css("color", "white");
 }
 
 function originalTheme() {
-  $("body").css("background-color", "white");
-  $(".col p, h1").css("color", "black");
-  $(".col a").css("color", "#0275d8");
-  $("footer").css("background-color", "#f5f5f5");
-  $("footer span").css("color", "black");
-  $("img").removeClass("svg-invert");
+    $("body").css("background-color", "white");
+    $("p, h1").css("color", "black");
+    $("a").css("color", "#0275d8");
+    $("footer span").css("color", "black");
+    $("code").css("color", "black");
 }
 
 /***/ }),
