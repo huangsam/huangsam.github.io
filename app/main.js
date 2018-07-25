7cb8ed192a2670ad406718a07a286be02459710b
Samuel Huang <samhuang91@gmail.com>
2018-07-24 22:36:55 -0700

    Update app formatting

--- app/main.js
+++ app/main.js
@@ -1,72 +1,72 @@
 /* Data structures */
 
 const serviceMeta = {
-    "github": {
-        "color": "#24292e",
-        "url": "https://github.com/huangsam/"
-    },
-    "facebook": {
-        "color": "#3b5998",
-        "url": "https://www.facebook.com/samuel.c.huang"
-    },
-    "linkedin": {
-        "color": "#0077b5",
-        "url": "https://www.linkedin.com/in/sambyte/"
-    },
-    "wordpress": {
-        "color": "#21759b",
-        "url": "https://sambyte.wordpress.com/"
-    }
-}
+  "github": {
+    "color": "#24292e",
+    "url": "https://github.com/huangsam/"
+  },
+  "facebook": {
+    "color": "#3b5998",
+    "url": "https://www.facebook.com/samuel.c.huang"
+  },
+  "linkedin": {
+    "color": "#0077b5",
+    "url": "https://www.linkedin.com/in/sambyte/"
+  },
+  "wordpress": {
+    "color": "#21759b",
+    "url": "https://sambyte.wordpress.com/"
+  }
+};
 
 /* Helper routines */
 
 function getServiceUrl(service) {
-    return serviceMeta[service]["url"]
+  return serviceMeta[service]["url"];
 }
 
 /* Event handlers */
 
 function changeTheme(service) {
-    let color = serviceMeta[service]["color"]
+  let color = serviceMeta[service]["color"];
 
-    $("body").css("background-color", color)
-    $("p, h1").css("color", "white")
-    $("a").css("color", "white")
-    $("footer span").css("color", "white")
-    $("pre").css("border", ".3em solid white")
+  $("body").css("background-color", color);
+  $("p, h1").css("color", "white");
+  $("a").css("color", "white");
+  $("footer span").css("color", "white");
+  $("pre").css("border", ".3em solid white");
 }
 
 function originalTheme() {
-    $("body").css("background-color", "white")
-    $("p, h1").css("color", "black")
-    $("a").css("color", "#0275d8")
-    $("footer span").css("color", "black")
-    $("pre").css("border", ".3em solid hsl(0, 0%, 33%)")
+  $("body").css("background-color", "white");
+  $("p, h1").css("color", "black");
+  $("a").css("color", "#0275d8");
+  $("footer span").css("color", "black");
+  $("pre").css("border", ".3em solid hsl(0, 0%, 33%)");
 }
 
 /* Event listeners */
 
 $("#service").change(function () {
-    let service = $(this).val()
-    if (service !== "default") {
-        changeTheme(service)
-    } else {
-        originalTheme()
-    }
-})
+  let service = $(this).val();
+  if (service !== "default") {
+    changeTheme(service);
+  } else {
+    originalTheme();
+  }
+});
 
 $("#goto").click(function (e) {
-    e.preventDefault()
-    let service = $("select#service").val()
-    if (service !== "default") {
-        window.location.href = getServiceUrl(service)
-    } else {
-        alert("Please select a valid service from the dropdown.")
-    }
-})
+  e.preventDefault();
+  let service = $("select#service").val();
+  if (service !== "default") {
+    window.location.href = getServiceUrl(service);
+  } else {
+    alert("Please select a valid service from the dropdown.");
+  }
+});
 
 $(document).ready(function () {
-    let currentYear = new Date().getFullYear()
-    $("#current-year").text(currentYear)
-})
+  let currentYear = new Date().getFullYear();
+  $("#current-year").text(currentYear);
+});
--- dist/bundle.js
+++ dist/bundle.js
@@ -99,73 +99,74 @@
 /* Data structures */
 
 var serviceMeta = {
-    "github": {
-        "color": "#24292e",
-        "url": "https://github.com/huangsam/"
-    },
-    "facebook": {
-        "color": "#3b5998",
-        "url": "https://www.facebook.com/samuel.c.huang"
-    },
-    "linkedin": {
-        "color": "#0077b5",
-        "url": "https://www.linkedin.com/in/sambyte/"
-    },
-    "wordpress": {
-        "color": "#21759b",
-        "url": "https://sambyte.wordpress.com/"
-    }
+  "github": {
+    "color": "#24292e",
+    "url": "https://github.com/huangsam/"
+  },
+  "facebook": {
+    "color": "#3b5998",
+    "url": "https://www.facebook.com/samuel.c.huang"
+  },
+  "linkedin": {
+    "color": "#0077b5",
+    "url": "https://www.linkedin.com/in/sambyte/"
+  },
+  "wordpress": {
+    "color": "#21759b",
+    "url": "https://sambyte.wordpress.com/"
+  }
+};
 
-    /* Helper routines */
+/* Helper routines */
 
-};function getServiceUrl(service) {
-    return serviceMeta[service]["url"];
+function getServiceUrl(service) {
+  return serviceMeta[service]["url"];
 }
 
 /* Event handlers */
 
 function changeTheme(service) {
-    var color = serviceMeta[service]["color"];
+  var color = serviceMeta[service]["color"];
 
-    $("body").css("background-color", color);
-    $("p, h1").css("color", "white");
-    $("a").css("color", "white");
-    $("footer span").css("color", "white");
-    $("pre").css("border", ".3em solid white");
+  $("body").css("background-color", color);
+  $("p, h1").css("color", "white");
+  $("a").css("color", "white");
+  $("footer span").css("color", "white");
+  $("pre").css("border", ".3em solid white");
 }
 
 function originalTheme() {
-    $("body").css("background-color", "white");
-    $("p, h1").css("color", "black");
-    $("a").css("color", "#0275d8");
-    $("footer span").css("color", "black");
-    $("pre").css("border", ".3em solid hsl(0, 0%, 33%)");
+  $("body").css("background-color", "white");
+  $("p, h1").css("color", "black");
+  $("a").css("color", "#0275d8");
+  $("footer span").css("color", "black");
+  $("pre").css("border", ".3em solid hsl(0, 0%, 33%)");
 }
 
 /* Event listeners */
 
 $("#service").change(function () {
-    var service = $(this).val();
-    if (service !== "default") {
-        changeTheme(service);
-    } else {
-        originalTheme();
-    }
+  var service = $(this).val();
+  if (service !== "default") {
+    changeTheme(service);
+  } else {
+    originalTheme();
+  }
 });
 
 $("#goto").click(function (e) {
-    e.preventDefault();
-    var service = $("select#service").val();
-    if (service !== "default") {
-        window.location.href = getServiceUrl(service);
-    } else {
-        alert("Please select a valid service from the dropdown.");
-    }
+  e.preventDefault();
+  var service = $("select#service").val();
+  if (service !== "default") {
+    window.location.href = getServiceUrl(service);
+  } else {
+    alert("Please select a valid service from the dropdown.");
+  }
 });
 
 $(document).ready(function () {
-    var currentYear = new Date().getFullYear();
-    $("#current-year").text(currentYear);
+  var currentYear = new Date().getFullYear();
+  $("#current-year").text(currentYear);
 });
 
 /***/ }),
