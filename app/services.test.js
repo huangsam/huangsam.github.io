c66523173da3ea408bacb0087d98e31cb1265daa
Samuel Huang <samhuang91@gmail.com>
2018-11-09 09:17:18 -0800

    Add describe block to jest tests

--- app/services.test.js
+++ app/services.test.js
@@ -2,36 +2,40 @@ require("./services");
 
 /* URL tests */
 
-test("github url works", () => {
-  expect(getServiceUrl("github")).toBe("https://github.com/huangsam/");
-});
+describe('All URLs should return as expected', () => {
+  test("github url works", () => {
+    expect(getServiceUrl("github")).toBe("https://github.com/huangsam/");
+  });
 
-test("facebook url works", () => {
-  expect(getServiceUrl("facebook")).toBe("https://www.facebook.com/samuel.c.huang");
-});
+  test("facebook url works", () => {
+    expect(getServiceUrl("facebook")).toBe("https://www.facebook.com/samuel.c.huang");
+  });
 
-test("linkedin url works", () => {
-  expect(getServiceUrl("linkedin")).toBe("https://www.linkedin.com/in/sambyte/");
-});
+  test("linkedin url works", () => {
+    expect(getServiceUrl("linkedin")).toBe("https://www.linkedin.com/in/sambyte/");
+  });
 
-test("wordpress url works", () => {
-  expect(getServiceUrl("wordpress")).toBe("https://sambyte.wordpress.com/");
+  test("wordpress url works", () => {
+    expect(getServiceUrl("wordpress")).toBe("https://sambyte.wordpress.com/");
+  });
 });
 
 /* Color tests */
 
-test("github color works", () => {
-  expect(getServiceColor("github")).toBe("#24292e");
-});
+describe('All colors should return as expected', () => {
+  test("github color works", () => {
+    expect(getServiceColor("github")).toBe("#24292e");
+  });
 
-test("facebook color works", () => {
-  expect(getServiceColor("facebook")).toBe("#3b5998");
-});
+  test("facebook color works", () => {
+    expect(getServiceColor("facebook")).toBe("#3b5998");
+  });
 
-test("linkedin color works", () => {
-  expect(getServiceColor("linkedin")).toBe("#0077b5");
-});
+  test("linkedin color works", () => {
+    expect(getServiceColor("linkedin")).toBe("#0077b5");
+  });
 
-test("wordpress color works", () => {
-  expect(getServiceColor("wordpress")).toBe("#21759b");
+  test("wordpress color works", () => {
+    expect(getServiceColor("wordpress")).toBe("#21759b");
+  });
 });
