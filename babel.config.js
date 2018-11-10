3e224b16797e168bffe0f645f91822a5c0e7c334
Samuel Huang <samhuang91@gmail.com>
2018-11-10 08:02:16 -0800

    Add node to targets

--- babel.config.js
+++ babel.config.js
@@ -7,20 +7,13 @@ const presets = [
         firefox: "60",
         chrome: "67",
         safari: "11.1",
+        node: "8",
       },
       useBuiltIns: "usage",
     },
   ],
 ];
 
-// https://github.com/facebook/jest/issues/2081
-const env = {
-  production: {
-    plugins: ["transform-es2015-modules-commonjs"]
-  }
-};
-
 module.exports = {
-  env,
   presets,
 };
