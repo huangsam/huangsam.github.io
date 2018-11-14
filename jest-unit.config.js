964c6e0a7c9c59bef82e9c8023549cbe4664fafd
Samuel Huang <samhuang91@gmail.com>
2018-11-14 08:18:11 -0800

    Add coverageThreshold

--- jest-unit.config.js
+++ jest-unit.config.js
@@ -2,4 +2,13 @@ const config = require('./jest.config');
 
 config.testRegex = 'app/[a-z]+\\.test\\.jsx?$';
 config.collectCoverage = true;
+config.coverageDirectory = 'coverage';
+config.coverageThreshold = {
+  global: {
+    branches: 80,
+    functions: 80,
+    lines: 80,
+    statements: -10,
+  },
+};
 module.exports = config;
