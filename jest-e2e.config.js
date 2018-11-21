8e68132a81454a188e0b8498ae84c04566ec4ceb
Samuel Huang <samhuang91@gmail.com>
2018-11-21 00:14:01 -0800

    Add line breaks for jest config

--- jest-e2e.config.js
+++ jest-e2e.config.js
@@ -2,4 +2,5 @@ const config = require('./jest.config');
 
 config.testRegex = 'app/[a-z]+\\.e2e\\.jsx?$';
 config.bail = true;
+
 module.exports = config;
--- jest-unit.config.js
+++ jest-unit.config.js
@@ -11,4 +11,5 @@ config.coverageThreshold = {
     statements: -10,
   },
 };
+
 module.exports = config;
