f32ce30c8314b7e49692e31d7f7479fe61f5a54d
Samuel Huang <samhuang91@gmail.com>
2019-07-30 07:58:29 -0700

    Use smaller webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
         },
       },
       {
