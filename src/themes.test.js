a151168b62ee72ed687064b47c99fe39fae1a2ac
Samuel Huang <samhuang91@gmail.com>
2020-04-05 12:00:30 -0700

    Update yarn dependencies and JS formatting

--- src/themes.test.js
+++ src/themes.test.js
@@ -9,12 +9,12 @@ describe('Theme logic should work as expected', () => {
     originalTheme();
     expect(document.body.style.backgroundColor).toBe('white');
     const pNodes = Array.from(document.getElementsByTagName('p'));
-    pNodes.forEach(node => {
+    pNodes.forEach((node) => {
       const { style } = node;
       expect(style.color).toBe('');
     });
     const preNodes = Array.from(document.getElementsByTagName('pre'));
-    preNodes.forEach(node => {
+    preNodes.forEach((node) => {
       const { style } = node;
       expect(style.border).toBe('');
     });
@@ -24,12 +24,12 @@ describe('Theme logic should work as expected', () => {
     customTheme('red');
     expect(document.body.style.backgroundColor).toBe('red');
     const pNodes = Array.from(document.getElementsByTagName('p'));
-    pNodes.forEach(node => {
+    pNodes.forEach((node) => {
       const { style } = node;
       expect(style.color).toBe('white');
     });
     const preNodes = Array.from(document.getElementsByTagName('pre'));
-    preNodes.forEach(node => {
+    preNodes.forEach((node) => {
       const { style } = node;
       expect(style.border).toBe('.3em solid white');
     });
