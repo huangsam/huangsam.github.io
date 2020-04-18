91e15c5b77af45611760bf44b6e044ed6f17d5de
Samuel Huang <samhuang91@gmail.com>
2020-04-18 15:44:44 -0700

    Fixup regex in services tests

--- src/services.test.js
+++ src/services.test.js
@@ -4,7 +4,7 @@ describe('Service colors should work as expected', () => {
   test.each(services.getServices())('%s color is valid', (svc) => {
     const color = services.getServiceColor(svc);
     expect(color).toBeTruthy();
-    expect(color).toMatch(/#[a-z0-9]{6}/);
+    expect(color).toMatch(/#[a-zA-Z0-9]{6}/);
   });
 
   test('bogus color throws error', () => {
@@ -18,7 +18,7 @@ describe('Service URLs should work as expected', () => {
   test.each(services.getServices())('%s url is valid', (svc) => {
     const url = services.getServiceUrl(svc);
     expect(url).toBeTruthy();
-    expect(url).toMatch(/https:\/\/.*/);
+    expect(url).toMatch(/https?:\/\/.*/);
   });
 
   test('bogus url throws error', () => {
