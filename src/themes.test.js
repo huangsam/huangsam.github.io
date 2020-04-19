5d252eebcf92fbf8cd2b49f8512bb0d14c242f7e
Samuel Huang <samhuang91@gmail.com>
2020-04-19 00:01:25 -0700

    Refactor tests to look readable in verbose

--- src/themes.test.js
+++ src/themes.test.js
@@ -1,41 +1,59 @@
 import themes from './themes';
 
+beforeAll(() => {
+  document.body.innerHTML = '<body><p></p><pre></pre></body>';
+});
+
 describe('Site themes', () => {
-  beforeEach(() => {
-    document.body.innerHTML = '<body><p></p><pre></pre></body>';
+  describe('Original theme', () => {
+    beforeAll(() => {
+      themes.originalTheme();
+    });
+
+    test('has white bg', () => {
+      expect(document.body.style.backgroundColor).toBe('white');
+    });
+
+    test('has plain text', () => {
+      const pNodes = Array.from(document.getElementsByTagName('p'));
+      pNodes.forEach((node) => {
+        const { style } = node;
+        expect(style.color).toBe('');
+      });
+    });
+
+    test('has plain borders', () => {
+      const preNodes = Array.from(document.getElementsByTagName('pre'));
+      preNodes.forEach((node) => {
+        const { style } = node;
+        expect(style.border).toBe('');
+      });
+    });
   });
 
-  test('original has white bg + plain text/borders', () => {
-    themes.originalTheme();
-    expect(document.body.style.backgroundColor).toBe('white');
-
-    const pNodes = Array.from(document.getElementsByTagName('p'));
-    pNodes.forEach((node) => {
-      const { style } = node;
-      expect(style.color).toBe('');
+  describe('Custom theme', () => {
+    beforeAll(() => {
+      themes.customTheme('red');
     });
 
-    const preNodes = Array.from(document.getElementsByTagName('pre'));
-    preNodes.forEach((node) => {
-      const { style } = node;
-      expect(style.border).toBe('');
-    });
-  });
-
-  test('custom has custom bg + white text/borders', () => {
-    themes.customTheme('red');
-    expect(document.body.style.backgroundColor).toBe('red');
-
-    const pNodes = Array.from(document.getElementsByTagName('p'));
-    pNodes.forEach((node) => {
-      const { style } = node;
-      expect(style.color).toBe('white');
+    test('has red bg', () => {
+      expect(document.body.style.backgroundColor).toBe('red');
     });
 
-    const preNodes = Array.from(document.getElementsByTagName('pre'));
-    preNodes.forEach((node) => {
-      const { style } = node;
-      expect(style.border).toBe('.3em solid white');
+    test('has white text', () => {
+      const pNodes = Array.from(document.getElementsByTagName('p'));
+      pNodes.forEach((node) => {
+        const { style } = node;
+        expect(style.color).toBe('white');
+      });
+    });
+
+    test('has white borders', () => {
+      const preNodes = Array.from(document.getElementsByTagName('pre'));
+      preNodes.forEach((node) => {
+        const { style } = node;
+        expect(style.border).toBe('.3em solid white');
+      });
     });
   });
 });
