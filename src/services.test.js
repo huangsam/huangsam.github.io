f87ee1d5e471dfee41b68f598befc45c21aadb5c
Samuel Huang <samhuang91@gmail.com>
2020-04-19 00:01:25 -0700

    Refactor tests to look readable in verbose

--- src/services.test.js
+++ src/services.test.js
@@ -1,27 +1,27 @@
 import services from './services';
 
-describe('Service colors', () => {
-  test.each(services.getServices())('color exists for %s', (svc) => {
+describe('Service color', () => {
+  test.each(services.getServices())('exists for %s', (svc) => {
     const color = services.getServiceColor(svc);
     expect(color).toBeTruthy();
     expect(color).toMatch(/#[a-zA-Z0-9]{6}/);
   });
 
-  test('bogus color throws TypeError', () => {
+  test('does not exist for bogus', () => {
     expect(() => {
       services.getServiceColor('bogus');
     }).toThrow(TypeError);
   });
 });
 
-describe('Service urls', () => {
-  test.each(services.getServices())('url exists for %s', (svc) => {
+describe('Service url', () => {
+  test.each(services.getServices())('exists for %s', (svc) => {
     const url = services.getServiceUrl(svc);
     expect(url).toBeTruthy();
     expect(url).toMatch(/https?:\/\/.*/);
   });
 
-  test('bogus url throws TypeError', () => {
+  test('does not exist for bogus', () => {
     expect(() => {
       services.getServiceUrl('bogus');
     }).toThrow(TypeError);
