7a9472662545b91931dfe0e382a47e7726933b28
Samuel Huang <samhuang91@gmail.com>
2020-04-19 21:57:25 -0700

    Replace Theme class with CSS rules

--- src/services.js
+++ src/services.js
@@ -1,34 +1,15 @@
-import Theme from './theme';
-
-// https://encycolorpedia.com/
 const serviceMeta = {
-  github: {
-    theme: Theme.CustomTheme('#171515'),
-    url: 'https://github.com/huangsam/',
-  },
-  facebook: {
-    theme: Theme.CustomTheme('#3b5998'),
-    url: 'https://www.facebook.com/samuel.c.huang',
-  },
-  linkedin: {
-    theme: Theme.CustomTheme('#0072b1'),
-    url: 'https://www.linkedin.com/in/sambyte/',
-  },
-  wordpress: {
-    theme: Theme.CustomTheme('#21759b'),
-    url: 'https://sambyte.wordpress.com/',
-  },
+  github: 'https://github.com/huangsam/',
+  facebook: 'https://www.facebook.com/samuel.c.huang',
+  linkedin: 'https://www.linkedin.com/in/sambyte/',
+  wordpress: 'https://sambyte.wordpress.com/',
 };
 
 // Helper methods
 const getServices = () => Object.keys(serviceMeta);
-const getServiceTheme = (service) => serviceMeta[service].theme;
-const getServiceUrl = (service) => serviceMeta[service].url;
-const getOriginalTheme = () => Theme.OriginalTheme();
+const getServiceUrl = (service) => serviceMeta[service];
 
 export default {
   getServices,
-  getServiceTheme,
   getServiceUrl,
-  getOriginalTheme,
 };
--- src/services.test.js
+++ src/services.test.js
@@ -1,18 +1,8 @@
 import services from './services';
-import Theme from './theme';
 
 describe('Service metadata', () => {
   test.each(services.getServices())('exists for %s', (svc) => {
-    const theme = services.getServiceTheme(svc);
-    expect(theme).toBeInstanceOf(Theme);
     const url = services.getServiceUrl(svc);
     expect(url).toMatch(/https?:\/\/.*/);
-  });
-});
-
-describe('Original theme', () => {
-  test('exists', () => {
-    const theme = services.getOriginalTheme();
-    expect(theme).toBeInstanceOf(Theme);
   });
 });
