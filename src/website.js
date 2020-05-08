4a29fed058562d69e40319ef3dc18f3dd7b35fe4
Samuel Huang <samhuang91@gmail.com>
2020-05-08 11:28:45 -0700

    Rename website variables

--- src/website.js
+++ src/website.js
@@ -6,19 +6,19 @@ const replaceNodeTheme = (node, themeA, themeB) => {
 export default class Website {
   constructor() {
     this.bodyNode = document.body;
-    this.paraNodes = document.querySelectorAll('p, h1, footer span');
-    this.preNodes = document.getElementsByTagName('pre');
+    this.textNodes = document.querySelectorAll('p, h1, footer span');
+    this.codeNodes = document.getElementsByTagName('pre');
     this.currentTheme = 'default';
   }
 
   setTheme(nextTheme) {
     replaceNodeTheme(this.bodyNode, this.currentTheme, nextTheme);
 
-    this.paraNodes.forEach((node) => {
+    this.textNodes.forEach((node) => {
       replaceNodeTheme(node, this.currentTheme, nextTheme);
     });
 
-    this.preNodes.forEach((node) => {
+    this.codeNodes.forEach((node) => {
       replaceNodeTheme(node, this.currentTheme, nextTheme);
     });
 
