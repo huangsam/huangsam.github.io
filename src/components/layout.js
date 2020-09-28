c00335f94794adc3c8feb12311c05572cb2bea4d
Samuel Huang <samhuang91@gmail.com>
2020-09-28 10:42:52 -0700

    Implement first draft of site

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from './layout/footer'
 import Header from './layout/header'
 
+import './layout.css'
+
 export default function Layout({children}) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className="content">{children}</main>
       <Footer />
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,74 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import {
+  FaTwitterSquare,
+  FaFacebookSquare,
+  FaGithub,
+  FaWordpress,
+  FaLinkedin,
+} from 'react-icons/fa';
 
-export default function Home() {
+const SOCIAL_COMPONENTS = {
+  linkedin: FaLinkedin,
+  twitter: FaTwitterSquare,
+  github: FaGithub,
+  facebook: FaFacebookSquare,
+  wordpress: FaWordpress,
+}
+
+export default function Home({ data }) {
+  const socialButtons = []
+  for (const obj of data.site.siteMetadata.social) {
+    socialButtons.push(React.createElement(
+      'a',
+      {key: obj.name, href: obj.url},
+      React.createElement(SOCIAL_COMPONENTS[obj.name])));
+  }
   return (
     <Layout>
-      <div>Hello World</div>
+      <div style={{
+        textAlign: `center`,
+        margin: `0 auto`,
+      }}>
+        <h1>Sam Huang</h1>
+        <p>Software Engineer at day. Artist at night.</p>
+        <div style={{
+          maxWidth: `300px`,
+          marginBottom: `1.45rem`,
+          marginLeft: `auto`,
+          marginRight: `auto`,
+        }}>
+          <Img
+            fluid={data.profileImg.childImageSharp.fluid}
+            imgStyle={{borderRadius: `50%`}}
+          />
+        </div>
+        <div className="social">
+          {socialButtons}
+        </div>
+      </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    profileImg: file(relativePath: { eq: "profile.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    site {
+      siteMetadata {
+        social {
+          name
+          url
+        }
+      }
+    }
+  }
+`;
