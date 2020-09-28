9ae0a834b90a3db3d873fcf371129e3e268a001d
Samuel Huang <samhuang91@gmail.com>
2020-09-27 22:47:00 -0700

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,28 @@
 module.exports = {
+  siteMetadata: {
+    title: `Sam's corner of the web`,
+    author: `Sam Huang`,
+    description: `A couple pages about Sam Huang`,
+    siteUrl: `https://sambyte.net/`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/huangsam`,
+      },
+      {
+        name: `facebook`,
+        url: `https://facebook.com/samuel.c.huang`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/sambyte/`,
+      },
+      {
+        name: `wordpress`,
+        url: `https://sambyte.wordpress.com/`,
+      },
+    ],
+  },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -7,5 +31,5 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-  ]
+  ],
 }
