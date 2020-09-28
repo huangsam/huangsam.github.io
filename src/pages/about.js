8263edf3f6962fb6244e78a7fbe1242fc740d994
Samuel Huang <samhuang91@gmail.com>
2020-09-28 12:57:28 -0700

    Refine content in About Me

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,11 +7,16 @@ export default function About({ data }) {
     <Layout>
       <SEO title="About" />
       <h1>About Me</h1>
-      <p>I graduated from UC Davis with an emphasis in Computer Science and Engineering.
-        Currently, I work as a Software Engineer at LinkedIn in Mountain View.</p>
-      <p>My primary hobbies are drumming and photography. I also enjoy doing ping-pong, hiking, traveling and foosball occasionally.</p>
-      <p>Currently, I go to two churches. The first one is in Daly City and it’s called Glorious Light Christian Church. The second one is in San Jose and it’s called Echo Church.</p>
-      <p>My parents are an important component of my life. They are the ones who shaped my life into who I am today, and I am happy to be their only son.</p>
+      <p>I graduated from <a href="https://www.ucdavis.edu/">UC Davis</a> with an emphasis in
+        Computer Science and Engineering. I currently work as a Software Engineer at
+        LinkedIn in Mountain View.</p>
+      <p>My primary hobbies are drumming and photography. I also enjoy doing ping-pong,
+        hiking, traveling and foosball occasionally.</p>
+      <p>Currently, I go to two churches. The first one is in Daly City and it’s
+        called <a href="http://glorious-light.net/">Glorious Light Christian Church</a>. The
+        second one is in San Jose and it’s called <a href="https://echo.church">Echo Church</a>.</p>
+      <p>My parents are an important component of my life. They are the ones who shaped my
+        life into who I am today, and I am happy to be their only son.</p>
     </Layout>
   )
 }
