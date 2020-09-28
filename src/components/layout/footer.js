58279f9428f792fb7c2073085028b99442491903
Samuel Huang <samhuang91@gmail.com>
2020-09-27 23:39:32 -0700

    Add minor formatting to layout

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>&copy; 2017-{new Date().getFullYear()}</footer>
+    <footer style={{marginTop: `1.45rem`}}>
+      <span>&copy; 2017-{new Date().getFullYear()}</span>
+    </footer>
   )
 }
