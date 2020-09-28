88da3d988fbe6d128bf79228452e0d143ffad7a7
Samuel Huang <samhuang91@gmail.com>
2020-09-28 13:01:09 -0700

    Remove unused param in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-export default function About({ data }) {
+export default function About() {
   return (
     <Layout>
       <SEO title="About" />
