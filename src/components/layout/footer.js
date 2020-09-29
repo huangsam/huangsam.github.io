028de4f54eb3373db72437e25174528250953962
Samuel Huang <samhuang91@gmail.com>
2020-09-28 19:02:45 -0700

    Apply prettier formatting

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -4,9 +4,12 @@ import { FaHeart } from "react-icons/fa"
 export default function Footer() {
   return (
     <footer>
-      &copy; 2016-{new Date().getFullYear()}&nbsp;
-      Made with <FaHeart style={{verticalAlign: `text-top`}}/>&nbsp;
-      by <a href="https://github.com/huangsam" style={{color: `black`}}>huangsam</a>
+      &copy; 2016-{new Date().getFullYear()}&nbsp; Made with{" "}
+      <FaHeart style={{ verticalAlign: `text-top` }} />
+      &nbsp; by{" "}
+      <a href="https://github.com/huangsam" style={{ color: `black` }}>
+        huangsam
+      </a>
     </footer>
   )
 }
