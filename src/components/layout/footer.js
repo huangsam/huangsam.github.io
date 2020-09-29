d0f52be0af03ec611b6110f36f300ae4d42257a5
Samuel Huang <samhuang91@gmail.com>
2020-09-28 19:39:21 -0700

    Rearrange footer content again

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -4,9 +4,8 @@ import { FaHeart } from "react-icons/fa"
 export default function Footer() {
   return (
     <footer>
-      &copy; 2016-{new Date().getFullYear()}&nbsp; Made with{" "}
-      <FaHeart style={{ verticalAlign: `text-top` }} />
-      &nbsp; by{" "}
+      &copy; 2016-{new Date().getFullYear()} Made with{" "}
+      <FaHeart style={{ verticalAlign: `text-top` }} /> by{" "}
       <a href="https://github.com/huangsam" style={{ color: `black` }}>
         huangsam
       </a>
