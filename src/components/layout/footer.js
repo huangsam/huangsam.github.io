55dc4a4a84e6c0183cea35f2ffdc858d301d1300
Samuel Huang <samhuang91@gmail.com>
2020-09-28 18:59:47 -0700

    Enhance footer content

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { FaHeart } from "react-icons/fa"
 
 export default function Footer() {
   return (
     <footer>
-      <span>&copy; 2016-{new Date().getFullYear()}</span>
+      &copy; 2016-{new Date().getFullYear()}&nbsp;
+      Made with <FaHeart style={{verticalAlign: `text-top`}}/>&nbsp;
+      by <a href="https://github.com/huangsam" style={{color: `black`}}>huangsam</a>
     </footer>
   )
 }
