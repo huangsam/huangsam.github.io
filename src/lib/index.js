2bcd0d88f3447a33fe1524e0aade42b2a96ccfe4
Samuel Huang <samhuang91@gmail.com>
2024-05-12 10:54:13 -0700

    Normalize strings

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,23 +1,23 @@
 export const socialProfiles = [
   {
-    name: `GitHub`,
-    url: `https://github.com/huangsam`
+    name: 'GitHub',
+    url: 'https://github.com/huangsam'
   },
   {
-    name: `Facebook`,
-    url: `https://facebook.com/samuel.c.huang`
+    name: 'Facebook',
+    url: 'https://facebook.com/samuel.c.huang'
   },
   {
-    name: `Linkedin`,
-    url: `https://www.linkedin.com/in/sambyte/`
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/sambyte/'
   },
   {
-    name: `Wordpress`,
-    url: `https://sambyte.wordpress.com/`
+    name: 'Wordpress',
+    url: 'https://sambyte.wordpress.com/'
   },
   {
-    name: `Twitter`,
-    url: `https://twitter.com/s_c_huang`
+    name: 'Twitter',
+    url: 'https://twitter.com/s_c_huang'
   }
 ];
 
