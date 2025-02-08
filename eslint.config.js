d21b426c62fe66a67ef5c61ee2ba6aa8d601719a
Samuel Huang <samhuang91@gmail.com>
2025-02-07 21:32:04 -0800

    Add linting voodoo for typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,34 @@
+import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import eslintPluginSvelte from 'eslint-plugin-svelte';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import { includeIgnoreFile } from '@eslint/compat';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-export default [
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ...eslintPluginSvelte.configs['flat/recommended'],
-  eslintConfigPrettier,
+  ...ts.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
   {
-    ignores: [
-      '.DS_Store',
-      'node_modules',
-      'build',
-      '.svelte-kit',
-      'package',
-      '.env',
-      '.env.*',
-      '!.env.example',
-      'pnpm-lock.yaml',
-      'package-lock.json',
-      'yarn.lock'
-    ]
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte'],
+
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser
+      }
+    }
   }
-];
+);
