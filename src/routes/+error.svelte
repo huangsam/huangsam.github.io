5267139e3ba8e57d976e906f5806550bbdce2365
Samuel Huang <samhuang91@gmail.com>
2025-02-09 07:51:10 -0800

    Apply more formatting tweaks

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,9 +5,17 @@
   $: error = ($page as Page).error;
 </script>
 
-{#if error}
-  <h1>{error.message}</h1>
-{:else}
-  <h1>Unknown error</h1>
-{/if}
-<p>Please return to the <a href="/">home page</a>.</p>
+<div class="error">
+  {#if error}
+    <h1>{error.message}</h1>
+  {:else}
+    <h1>Unknown error</h1>
+  {/if}
+  <p>Please return to the <a href="/">home page</a>.</p>
+</div>
+
+<style>
+  div.error {
+    padding: 1em 1.25em;
+  }
+</style>
