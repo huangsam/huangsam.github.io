9fe597ffeaf3b4659ea3a9e15059bc9f2dedd898
Samuel Huang <samhuang91@gmail.com>
2025-03-01 21:21:49 -0800

    Update site dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
-import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
 import ts from 'typescript-eslint';
+
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+
+import svelteConfig from './svelte.config.js';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
@@ -28,6 +32,7 @@ export default ts.config(
     languageOptions: {
       parserOptions: {
         parser: ts.parser,
+        svelteConfig,
       },
     },
   },
