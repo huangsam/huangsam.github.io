d7192fa85c6984a78ead58ef1a16ff82396fc9c0
Samuel Huang <samhuang91@gmail.com>
2025-06-13 05:24:06 -0700

    Remove enhancedImg from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [enhancedImages(), sveltekit()],
+  plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
