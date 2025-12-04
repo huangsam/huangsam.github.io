346d2a9d62e34ab0ef937bbf812d45b67351e191
Samuel Huang <samhuang91@gmail.com>
2025-12-04 08:14:36 -0800

    Extend profile image with CSS variables

--- src/lib/components/ProfileImage.svelte
+++ src/lib/components/ProfileImage.svelte
@@ -12,8 +12,9 @@
   }
   .profile-image {
     border-radius: 25%;
-    width: 21rem;
-    height: 21rem;
+    /* Default size is 21rem, can be overridden by CSS variable */
+    width: var(--profile-image-size, 21rem);
+    height: var(--profile-image-size, 21rem);
     box-shadow: 3px 3px 5px lightgrey;
     -moz-box-shadow: 3px 3px 5px lightgrey;
     -webkit-box-shadow: 3px 3px 5px lightgrey;
@@ -21,8 +22,9 @@
   }
   @media (max-width: 768px) {
     .profile-image {
-      width: 19rem;
-      height: 19rem;
+      /* Smaller size for mobile, can be overridden by CSS variable */
+      width: var(--profile-image-size-small, 19rem);
+      height: var(--profile-image-size-small, 19rem);
     }
   }
 </style>
