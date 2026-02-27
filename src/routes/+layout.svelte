12998bb78fb1c3a43d41fcf98b6c863f82cf4d41
Samuel Huang <samhuang91@gmail.com>
2026-02-27 07:50:45 -0800

    Make sticky footer robust across the site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,20 +14,22 @@
   <meta name="description" content="Website for Sam Huang" />
 </svelte:head>
 
-<header>
-  <a href="/">Sam's website</a>
-  <nav class="right">
-    <a href="https://github.com/huangsam/huangsam.github.io">Source code</a>
-  </nav>
-</header>
+<div class="layout">
+  <header>
+    <a href="/">Sam's website</a>
+    <nav class="right">
+      <a href="https://github.com/huangsam/huangsam.github.io">Source code</a>
+    </nav>
+  </header>
 
-<main>
-  <slot />
-</main>
+  <main>
+    <slot />
+  </main>
 
-<footer>
-  <span>&copy;</span>
-  <span>2016-{now.getFullYear()}</span>
-  <span> | </span>
-  <span>Made with &hearts; by @huangsam.</span>
-</footer>
+  <footer>
+    <span>&copy;</span>
+    <span>2016-{now.getFullYear()}</span>
+    <span> | </span>
+    <span>Made with &hearts; by @huangsam.</span>
+  </footer>
+</div>
