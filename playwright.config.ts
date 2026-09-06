df05f3c8392f9222b47c3bc396cc151522320f37
Samuel Huang <samhuang91@gmail.com>
2026-09-06 08:06:26 -0700

    Add a new command to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,7 +64,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm preview',
+    command: 'pnpm build && pnpm preview',
     url: 'http://localhost:4173',
     reuseExistingServer: !process.env.CI,
   },
